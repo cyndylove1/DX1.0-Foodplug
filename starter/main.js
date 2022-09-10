becc0e6b25a7a55ec7ea1ac4087e4d90536a1fe9
cynthia nweke <nwekecynthia2015@gmail.com>
2022-09-10 13:00:56 +0100

    completed project section

---
+++ starter/main.js
@@ -0,0 +1,24 @@
+
+
+// basic Es6 function decalaration
+//function name(){}
+var myname = "nweke Cynthia"
+
+function popsomething() {
+    alert(myname + "  i have been coding since 12")
+}
+//console.logo(myname)
+
+console.log(document)
+
+var ourbody=document.querySelector(".ourpagebody")
+
+// ourbody.style.background="black"
+var hamburger=document.querySelector(".hamburger-container")
+
+console.log(hamburger)
+
+function addandremove(){
+    hamburger.classList.toggle('showburger')
+}
+
